1e6140c68075f310e9a1694edfaaf4021ba914f2
shoti <shoti@DESKTOP-6ORT5S5>
2023-07-01 00:34:14 +0300

    Добавьте файлы проекта.

---
+++ Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Notes.Persistence
+{
+    internal class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ Notes.Persistence/EntityTypeConfiguration/NoteConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Notes.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Notes.Persistence.EntityTypeConfiguration
+{
+    internal class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id);
+            builder.HasIndex(note => note.Id).IsUnique();
+            builder.Property(note => note.Title).IsRequired().HasMaxLength(250);
+        }
+    }
+}
